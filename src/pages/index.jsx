dfc6696532d894d65e8d8bd12a4d873c9bade4da
zismaildev <nattapong130247@gmail.com>
2024-01-22 00:56:52 +0700

    add content add regteam page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -26,7 +26,7 @@ export default function Home() {
                 <h4 className="text-xl text-gray-600 mt-2">ชมรม E-Spout มหาวิทยาลัยราชภัฏเชียงใหม่</h4>
                 <div className='text-left mt-5'>
                     <h2 className='text-xl text-gray-600 mt-2'>
-                        สวัสดีทุกคนในชุมชน CMRU E-Sport เหนื่อยจากการต่อสู้ในโลกธรรมดาแล้วหรือยัง? ไม่เป็นไร!
+                        สวัสดีทุกคนในชุมชน CMRU เหนื่อยจากการต่อสู้ในโลกธรรมดาแล้วหรือยัง? ไม่เป็นไร!
                         เพราะเราขอเชิญทุกท่านมาร่วมเป็นส่วนหนึ่งของครอบครัว CMRU E-Sport กัน!  สิ่งที่คุณจะได้
                     </h2>
                     <ul className="text-xl text-gray-600 list-disc pl-5">
@@ -40,61 +40,64 @@ export default function Home() {
                     <h2 className="text-xl text-gray-600 mt-2">
                         เรารอคอยทุกคนมาเป็นส่วนหนึ่งของชุมชน CMRU E-Sport และเตรียมพบกันในวันงาน! #CMRUESport #GamingCommunity #สนุกสนาน #ร่วมสนุกกับเกม
                     </h2>
-                </div>
 
-                <h2 className="text-3xl font-bold tracking-tight sm:text-4xl mt-10">Competitive Atmosphere</h2>
-                <h4 className="text-xl text-gray-600">บรรยากาศการแข่งขัน</h4>
-                <div className="gap-4 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 mt-5">
-                    <div className='text-left mt-5'>
-                        <p className="text-xl text-gray-600 mt-2">
-                            ภาพบรรยากาศกิจกรรมแข่งขันเกม Rov ที่สุดของเวียงพิงค์เกม ครั้งที่ 33 ที่จัดขึ้นที่ตึก 90 มหาวิทยาลัยราชภัฏเชียงใหม่
-                            การแข่งขันนี้มีความสนุกสนานและความเป็นมืออาชีพในการแข่งขัน E-Sport จากนักกีฬาจากมหาวิทยาลัยราชภัฏกลุ่มภาคเหนือ
-                            ที่มาเข้าร่วมการแข่งขันกีฬา E-Sport เพื่อแสดงฝีมือและเทคนิคในการเล่นเกมของพวกเขา
-                        </p>
-                        <p className="text-xl text-gray-600 mt-2">
-                            ค้นพบรอบการแข่งขันที่น่าตื่นเต้นและสุดมันส์ ทั้งการประลองทักษะทางเทคนิคและการต่อสู้ที่คับคั่ง
-                            ร่วมสัมผัสบรรยากาศที่เต็มไปด้วยความสนุกและความท้าทาย ทั้งจากทีมทีมงานและผู้ชมทุกคน
-                        </p>
-                    </div>
-                    <div>
-                        <iframe
-                            width="100%"
-                            height="315"
-                            src="https://www.youtube.com/embed/12VmXrFQTPk"
-                            title="YouTube video player"
-                            frameBorder="0"
-                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                            allowFullScreen
-                        ></iframe>
-
-                    </div>
-                </div>
-
-                <h2 className="text-3xl font-bold tracking-tight sm:text-4xl mt-10">Competition game</h2>
-                <h4 className="text-xl text-gray-600">เกมการแข่งขัน</h4>
-                <div className="gap-4 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3">
-                    {GameListData.map((item, index) => (
-                        <Link key={index} href={`/games/${encodeURIComponent(item.title)}`}>
-                            <Card className="group shadow-sm transition-transform transform duration-300 ease-in-out hover:scale-105 bg-transparent">
-                                <CardBody className="overflow-visible p-5 justify-between">
-                                    <Image
-                                        shadow="sm"
-                                        radius="lg"
-                                        width="100%"
-                                        height="auto"
-                                        alt={item.title}
-                                        className="w-full object-cover h-[260px]"
-                                        src={item.img}
-                                    />
-                                    <h1 className="text-left font-bold tracking-tight text-sm 2xl:text-md xl:text-lg lg:text-xl md:text-2xl sm:text-3xl mt-1">{item.title}</h1>
-                                    <p>{item.contents}</p>
-                                </CardBody>
-                            </Card>
-                        </Link>
-                    ))}
-                </div>
-
+                    <h2 className="text-xl text-gray-600 mt-2">
+                        สามารถ <Link color="foreground" href="/contents/regteam">สมัครแข่งขัน</Link> ตรงนี้ !!!
+                    </h2>
             </div>
+
+            <h2 className="text-3xl font-bold tracking-tight sm:text-4xl mt-10">Competitive Atmosphere</h2>
+            <h4 className="text-xl text-gray-600">บรรยากาศการแข่งขัน</h4>
+            <div className="gap-4 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 mt-5">
+                <div className='text-left mt-5'>
+                    <p className="text-xl text-gray-600 mt-2">
+                        ภาพบรรยากาศกิจกรรมแข่งขันเกม Rov ที่สุดของเวียงพิงค์เกม ครั้งที่ 33 ที่จัดขึ้นที่ตึก 90 มหาวิทยาลัยราชภัฏเชียงใหม่
+                        การแข่งขันนี้มีความสนุกสนานและความเป็นมืออาชีพในการแข่งขัน E-Sport จากนักกีฬาจากมหาวิทยาลัยราชภัฏกลุ่มภาคเหนือ
+                        ที่มาเข้าร่วมการแข่งขันกีฬา E-Sport เพื่อแสดงฝีมือและเทคนิคในการเล่นเกมของพวกเขา
+                    </p>
+                    <p className="text-xl text-gray-600 mt-2">
+                        ค้นพบรอบการแข่งขันที่น่าตื่นเต้นและสุดมันส์ ทั้งการประลองทักษะทางเทคนิคและการต่อสู้ที่คับคั่ง
+                        ร่วมสัมผัสบรรยากาศที่เต็มไปด้วยความสนุกและความท้าทาย ทั้งจากทีมทีมงานและผู้ชมทุกคน
+                    </p>
+                </div>
+                <div>
+                    <iframe
+                        width="100%"
+                        height="315"
+                        src="https://www.youtube.com/embed/12VmXrFQTPk"
+                        title="YouTube video player"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen
+                    ></iframe>
+
+                </div>
+            </div>
+
+            <h2 className="text-3xl font-bold tracking-tight sm:text-4xl mt-10">Competition game</h2>
+            <h4 className="text-xl text-gray-600">เกมการแข่งขัน</h4>
+            <div className="gap-4 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3">
+                {GameListData.map((item, index) => (
+                    <Link key={index} href={`/games/${encodeURIComponent(item.title)}`}>
+                        <Card className="group shadow-sm transition-transform transform duration-300 ease-in-out hover:scale-105 bg-transparent">
+                            <CardBody className="overflow-visible p-5 justify-between">
+                                <Image
+                                    shadow="sm"
+                                    radius="lg"
+                                    width="100%"
+                                    height="auto"
+                                    alt={item.title}
+                                    className="w-full object-cover h-[260px]"
+                                    src={item.img}
+                                />
+                                <h1 className="text-left font-bold tracking-tight text-sm 2xl:text-md xl:text-lg lg:text-xl md:text-2xl sm:text-3xl mt-1">{item.title}</h1>
+                                <p>{item.contents}</p>
+                            </CardBody>
+                        </Card>
+                    </Link>
+                ))}
+            </div>
+
         </div>
+        </div >
     )
 }
