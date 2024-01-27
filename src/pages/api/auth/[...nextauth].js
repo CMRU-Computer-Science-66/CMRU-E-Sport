1855562b7d421ca21c46b2ffc03303eaf2e4d825
Faelayis <48393914+Faelayis@users.noreply.github.com>
2024-01-27 19:20:54 +0700

    Add `await` NextAuth

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -3,7 +3,7 @@ import NextAuth from "next-auth";
 import prisma from '@cmru-comsci-66/e-sport-database'
 import { NextAuthOptions } from '@cmru-comsci-66/e-sport-nextjs-server';
 
-export default NextAuth(NextAuthOptions(PrismaAdapter(prisma), {
+export default NextAuth(await NextAuthOptions(PrismaAdapter(prisma), {
       GoogleProvider: {
          clientId: process.env.GOOGLE_CLIENT_ID,
          clientSecret: process.env.GOOGLE_CLIENT_SECRET
