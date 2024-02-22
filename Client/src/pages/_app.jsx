53f21dad9972d232ae9e1aff5bececb149e4d1df
Faelayis <48393914+Faelayis@users.noreply.github.com>
2024-02-23 01:41:07 +0700

    Fix reload page screen flickers

--- Client/src/pages/_app.jsx
+++ Client/src/pages/_app.jsx
@@ -5,8 +5,7 @@ import Providers from "@/pages/providers";
 import { Navbar } from "@/components/navbar";
 import { Link } from "@nextui-org/link";
 import clsx from "clsx";
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { SessionProvider } from "next-auth/react";
 
 export const metadata = {
@@ -36,33 +35,37 @@ export default function App({
 		setIsClient(true);
 	}, []);
 
-	if (!isClient) {
-		return null;
-	}
-
-	return (
-		<body
-			className={clsx(
-				"min-h-screen bg-background font-sans antialiased",
-				fontSans.variable,
-			)}
-		>
-			<SessionProvider session={session}>
-				<Providers themeProps={{ attribute: "class", defaultTheme: "system" }}>
-					<div className="relative flex h-screen flex-col">
-						<Navbar />
-						<main className="container mx-auto max-w-7xl flex-grow px-6 pt-16">
-							<Component {...pageProps} />
-						</main>
-						<footer className="flex w-full items-center justify-center py-3">
-							<Link isExternal className="flex items-center gap-1 text-current">
-								<span className="text-default-600">จัดทำโดย</span>
-								<p className="text-primary">CMRU E Sport</p>
-							</Link>
-						</footer>
-					</div>
-				</Providers>
-			</SessionProvider>
-		</body>
+	const Main = (
+		<SessionProvider session={session}>
+			<Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
+				<div className="relative flex h-screen flex-col">
+					<Navbar />
+					<main className="container mx-auto max-w-7xl flex-grow px-6 pt-16">
+						<Component {...pageProps} />
+					</main>
+					<footer className="flex w-full items-center justify-center py-3">
+						<Link isExternal className="flex items-center gap-1 text-current">
+							<span className="text-default-600">จัดทำโดย</span>
+							<p className="text-primary">CMRU E Sport</p>
+						</Link>
+					</footer>
+				</div>
+			</Providers>
+		</SessionProvider>
 	);
+
+	if (!isClient) {
+		return Main;
+	} else {
+		return (
+			<body
+				className={clsx(
+					"min-h-screen bg-background font-sans antialiased",
+					fontSans.variable,
+				)}
+			>
+				{Main}
+			</body>
+		);
+	}
 }
